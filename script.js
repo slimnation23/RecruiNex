108b91259d90a583cfbc7e762c004a91daaa68bb
slimnation23 <svetaberynda@gmail.com>
2023-08-16 19:52:36 +0200

    header2

--- script.js
+++ script.js
@@ -4,8 +4,13 @@ const navList = document.querySelector(".nav-list");
 const navLink = document.querySelectorAll(".nav-link");
 const header = document.querySelector(".header");
 const nav = document.querySelector(".nav");
+const btn = document.querySelector(".contact-btn");
+
 
 menuBtn.addEventListener("click", toggleMenu);
+btn.addEventListener("click", () => {
+    alert(123)
+});
 
 function toggleMenu() {
   navList.classList.toggle("active");
