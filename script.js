aaeeec5b2fedc880ad77bec3546f5de1842f2c2a
slimnation23 <svetaberynda@gmail.com>
2023-08-16 16:31:26 +0200

    mobileNavDone

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const closeBtn = document.querySelector(".menu-close");
 const navList = document.querySelector(".nav-list");
 const navLink = document.querySelectorAll(".nav-link");
 const header = document.querySelector(".header");
+const nav = document.querySelector(".nav");
 
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   navList.classList.toggle("active");
   header.classList.toggle("open");
-  
+  nav.style.borderRadius = '0'
+
   navLink.forEach((item) => {
     item.addEventListener("click", () => {
       navList.classList.remove("active");
